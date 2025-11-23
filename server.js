89ff3d437f3449e12429322103cb5eb90f9b3ebf
Sangaychoden <12220059.gcit2rub.edu.bt>
2025-11-24 02:16:41 +0600

    render link update

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "https://thimdorjiwebsite.onrender.com",
+      "https://thimdorjiadmin.onrender.com",
       "https://thim-dorji-frontend.vercel.app",
       "https://thim-dorji-frontend-ami1.vercel.app",
       "https://thim-dorji-frontend-m6uj.vercel.app"
